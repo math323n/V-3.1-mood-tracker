e999fe25281f11acc9c38b7a8df79b01c8e32a00
Your Name <math323n@edu.campusvejle.dk>
2021-01-06 11:27:56 +0100

    added button

---
+++ src/components/buttons/submitButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button
+    class="px-5 py-3 self-end text-green-50 bg-green-800 hover:bg-green-600 rounded transition-colors"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+
+  props: {
+    label: String,
+  },
+};
+</script>
